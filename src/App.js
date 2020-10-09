f3369fdfadfe0245eaa62f42bb134545b59b75eb
Your Name <mihalaras11@gmail.com>
2020-10-10 01:28:05 +0300

    added basic router functionality

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ import "./styles/app.scss";
 // Redux
 import { connect } from 'react-redux'
 import { stopLoading } from './redux/actions/testAction'
+//Router
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App({ loading, stopLoading }) {
 
   setTimeout(stopLoading, 1000)
 
   return (
-    <div>
-	{
-	  loading
-	    ? <h1>Loading...</h1>
-	    : <h1>Redux Success</h1>
-	}
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <h1>you are at /</h1>
+          </Route>
+          <Route exact path="/testroute">
+            <h1>you are at /testRoute</h1>
+          </Route>
+        </Switch>
+        {
+          loading
+            ? <h1>Loading...</h1>
+            : <h1>Redux Success</h1>
+        }
+
+      </div>
+    </BrowserRouter>
+
   )
 }
 
