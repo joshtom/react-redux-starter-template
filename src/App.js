2be88875b11bbd024677581c15091c9c233b9a5b
aldo-rl <aldorl.mee@gmail.com>
2020-10-09 14:30:59 -0500

    [ADD] redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from "react";
 import "./styles/app.scss";
 
-function App() {
-  return <div></div>;
+
+// Redux
+import { connect } from 'react-redux'
+import { stopLoading } from './redux/actions/testAction'
+
+function App({ loading, stopLoading }) {
+
+  setTimeout(stopLoading, 1000)
+
+  return (
+    <div>
+	{
+	  loading
+	    ? <h1>Loading...</h1>
+	    : <h1>Redux Success</h1>
+	}
+    </div>
+  )
 }
 
-export default App;
+const mapStateToProps = ({ loading }) => {
+  return {
+    loading: loading.loading
+  }
+}
+
+const mapDispatchToProps = {
+  stopLoading,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
