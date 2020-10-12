f57312622d0ed717497abdcb033846ebac99fb5a
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-10-12 18:55:00 +0100

    I add a counter class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./styles/app.scss";
 
-
 // Redux
-import { connect } from 'react-redux'
-import { stopLoading } from './redux/actions/testAction'
+import { connect } from "react-redux";
+import { stopLoading } from "./redux/actions/testAction";
 //Router
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 // Pages
 import NotFound from "./pages";
-
+import CounterPage from "./pages/Counter";
 function App({ loading, stopLoading }) {
-
-  setTimeout(stopLoading, 1000)
+  setTimeout(stopLoading, 1000);
 
   return (
     <BrowserRouter>
@@ -22,29 +20,25 @@ function App({ loading, stopLoading }) {
             <h1>you are at /</h1>
           </Route>
           <Route exact path="/testroute">
-            {
-              loading
-                  ? <h1>Loading...</h1>
-                  : <h1>Redux Success</h1>
-            }
+            {loading ? <h1>Loading...</h1> : <h1>Redux Success</h1>}
             <h1>you are at /testRoute</h1>
           </Route>
-          <Route component={NotFound}/>
+          <Route exact path="/counter" component={CounterPage} />
+          <Route exact component={NotFound} />
         </Switch>
       </div>
     </BrowserRouter>
-
-  )
+  );
 }
 
 const mapStateToProps = ({ loading }) => {
   return {
-    loading: loading.loading
-  }
-}
+    loading: loading.loading,
+  };
+};
 
 const mapDispatchToProps = {
   stopLoading,
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/pages/Counter/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class CounterPage extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main>
+          <p>This is a class Component</p>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default CounterPage;
