ab141a4ba2910a898bc11c11c6330006ed1aeb87
Tailine Chagas <tailinechagas@gmail.com>
2020-10-18 18:37:22 -0300

    feat: add interaction to counter functional component with useSelector and useDispatch hooks

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { decreaseValue } from '../../redux/actions/counterActions';
+import { increaseValue } from '../../redux/actions/counterActions/index';
 
 export function Counter() {
+
+  const counterValue = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <React.Fragment>
       <div className="counter-class">
         <div className="counter-class__container">
           <div
+            onClick={() => dispatch(decreaseValue())}
             className="counter-class__container--leftArrow"
           >
             {" "}
@@ -18,8 +25,9 @@ export function Counter() {
               <path d="M24.078 26.457a2.5 2.5 0 01-1.77 4.267 2.497 2.497 0 01-1.768-.731L5.914 15.362 20.543.732a2.499 2.499 0 113.535 3.536L12.984 15.362l11.094 11.095z" />
             </svg>{" "}
           </div>
-          <div className="counter-class__container--box">0</div>
+          <div className="counter-class__container--box">{counterValue}</div>
           <div
+            onClick={() => dispatch(increaseValue())}
             className="counter-class__container--rightArrow"
           >
             <svg
