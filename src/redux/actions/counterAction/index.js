c4eea6aa5062c5c12a3d2e83acea2f9094ea2939
joshtom <olajidejoshua4real@gmail.com>
2020-10-19 11:17:54 +0100

    Delete redundant files / folders

--- src/redux/actions/counterAction/index.js
+++
@@ -1,9 +0,0 @@
-import { DECREMENT, INCREMENT } from '../../actionsTypes/counterActionTypes'
-
-export const incrementCounter = () => {
-  return { type: INCREMENT }
-}
-
-export const decrementCounter = () => {
-  return { type: DECREMENT }
-}
